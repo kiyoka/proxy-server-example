073471ffe201f1cb2626ea4ee28be784257b50a9
Kiyoka Nishiyama <kiyoka@sumibi.org>
2020-07-31 22:03:21 +0900

    success to access http://info.cern.ch url via this proxy version.

--- http-proxy/http-proxy.py
+++ http-proxy/http-proxy.py
@@ -1,16 +1,56 @@
 # -*- coding: utf-8 -*-
+#
+# a simple http only proxy server.
+#
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.client import HTTPConnection
+from urllib.parse import urlparse
 
 class ProxyRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+
+    def get_remote_content(self):
         req = self
         content_length = req.headers.get('Content-Length', 0)
-        print('  Content-Length: {}'.format(content_length))
-        self.send_response(200)
-        self.send_header('Content-Length', 5)
-        self.end_headers()
-        self.wfile.write("body\n".encode('utf-8'))
+
+        path = req.path
+        host = req.headers['Host']
+        obj  = urlparse(path)
+
+        print('  host: {}'.format(host))
+        print('  path: {}'.format(path))
+        print('  scheme: {}'.format(obj.scheme))
+        print('  port: {}'.format(obj.port))
+
+        if(obj.scheme == 'http'):
+            try:
+                conn = HTTPConnection(host,obj.port)
+                req_body = self.rfile.read(content_length) if content_length else None
+                conn.set_debuglevel(1)
+                conn.request('GET', path, req_body, dict(req.headers))
+                res = conn.getresponse()
+                res_body = res.read()
+                return (True,res_body)
+            except Exception as e:
+                print(e)
+                self.send_error(502)
+                return (False,'')
+        else:
+            return (False,'')
+
+    def do_GET(self):
+        req = self
+        (flag,res_body) = self.get_remote_content()
+        if flag:
+            self.send_response(200)
+            content_length = int(len(res_body))
+            print('  length of remote contens: {}'.format(content_length))
+            self.send_header('Content-Length', content_length)
+            self.end_headers()
+            self.wfile.write(res_body)
+            self.wfile.flush()
+        else:
+            self.send_error(501,'Unsupported')
 
     do_HEAD = do_GET
     do_POST = do_GET
@@ -19,7 +59,7 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
     do_OPTIONS = do_GET
 
 def run(handler_class=ProxyRequestHandler, server_class=HTTPServer, protocol="HTTP/1.1"):
-    port = 8000
+    port = 8080
     server_address = ('',port)
     handler_class.protocol_version = protocol
     httpd = server_class(server_address, handler_class)
